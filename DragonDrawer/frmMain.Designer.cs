2ffdf599b9142a222f57bf289f7a9402cd51ef6f
Akihiro Suzuki <asuzuki@estrella.co.jp>
2024-01-04 09:07:38 +0900

    プロジェクト ファイルを追加します。

---
+++ DragonDrawer/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace DragonDrawer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.nudLevel = new System.Windows.Forms.NumericUpDown();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.btnAnimation = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLevel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(250, 12);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "固定描画";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Location = new System.Drawing.Point(12, 17);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(173, 12);
+            this.lblCaption.TabIndex = 1;
+            this.lblCaption.Text = "回帰の回数を入力してね(0は直線)";
+            // 
+            // nudLevel
+            // 
+            this.nudLevel.Location = new System.Drawing.Point(191, 15);
+            this.nudLevel.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudLevel.Name = "nudLevel";
+            this.nudLevel.Size = new System.Drawing.Size(53, 19);
+            this.nudLevel.TabIndex = 2;
+            // 
+            // picMain
+            // 
+            this.picMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picMain.Location = new System.Drawing.Point(12, 41);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(776, 397);
+            this.picMain.TabIndex = 3;
+            this.picMain.TabStop = false;
+            // 
+            // btnAnimation
+            // 
+            this.btnAnimation.Location = new System.Drawing.Point(331, 12);
+            this.btnAnimation.Name = "btnAnimation";
+            this.btnAnimation.Size = new System.Drawing.Size(75, 23);
+            this.btnAnimation.TabIndex = 4;
+            this.btnAnimation.Text = "動きます";
+            this.btnAnimation.UseVisualStyleBackColor = true;
+            this.btnAnimation.Click += new System.EventHandler(this.btnAnimation_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(412, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "止まります";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnAnimation);
+            this.Controls.Add(this.picMain);
+            this.Controls.Add(this.nudLevel);
+            this.Controls.Add(this.lblCaption);
+            this.Controls.Add(this.btnDraw);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "フラクタルで辰を描きます";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudLevel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label lblCaption;
+        private System.Windows.Forms.NumericUpDown nudLevel;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Button btnAnimation;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ DragonDrawer/frmMain.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragonDrawer
+{
+    public partial class frmMain : Form
+    {
+        /// <summary>
+        /// 胴体の角度
+        /// </summary>
+        private double m_currentAngle = 0;
+
+        /// <summary>
+        /// 胴体の各部位の座標
+        /// </summary>
+        private List<PointF> m_points = new List<PointF>();
+
+        /// <summary>
+        /// 胴体の各部位の角度
+        /// </summary>
+        private List<double> m_angles = new List<double>();
+
+        /// <summary>
+        /// ペンの太さ(最低限辰と認識できる大きさ)
+        /// </summary>
+        private int m_penSize = 5;
+
+        /// <summary>
+        /// 身体のメインカラー
+        /// </summary>
+        private Color m_bodyMain = Color.FromArgb(34, 177, 76);
+
+        /// <summary>
+        /// 身体の暗い所(背中等)
+        /// </summary>
+        private Color m_bodyDark = Color.FromArgb(24, 128, 55);
+
+        /// <summary>
+        /// 目口鼻
+        /// </summary>
+        private Color m_eyeAndMouthAndNose = Color.FromArgb(0, 0, 0);
+
+        /// <summary>
+        /// 角とひげ
+        /// </summary>
+        private Color m_antlersAndBeard = Color.FromArgb(239, 228, 176);
+
+        /// <summary>
+        /// 尻尾
+        /// </summary>
+        private Color m_Tail = Color.FromArgb(255, 0, 0);
+
+        /// <summary>
+        /// 空
+        /// </summary>
+        private Color m_sky = Color.LightSkyBlue;
+
+        /// <summary>
+        /// 太陽
+        /// </summary>
+        private Color m_sun = Color.Yellow;
+
+        /// <summary>
+        /// 雲
+        /// </summary>
+        private Color m_cloud = Color.White;
+
+        /// <summary>
+        /// 描画タスク
+        /// </summary>
+        private Thread m_moveTask;
+
+        /// <summary>
+        /// タスクの状態
+        /// </summary>
+        private bool m_taskMoving = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 読み込みイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 描画メインイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (m_taskMoving == false)
+                {
+                    InitImage();
+
+                    // 辰の描画
+                    MakeDragonBody((int)(nudLevel.Value), picMain.Width);
+                    DrawDragonBody(picMain.Image,m_points);
+                    DrawDragonHead(picMain.Image, m_points.First());
+                    DrawDragonTail(picMain.Image, m_points.Last());
+
+                    MessageBox.Show("あけましておめでとうございます。");
+                }
+                else
+                {
+                    MessageBox.Show("あけましておめでとうございます。辰を止めてください。");
+                }
+
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("あけましておめでとうございます。：エラーです。" + _ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 辰が流れるモード
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAnimation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (m_taskMoving == false)
+                {
+                    InitImage();
+
+                    // 先に座標を計算する
+                    MakeDragonBody((int)(nudLevel.Value), picMain.Width);
+                    m_taskMoving = true;
+                    m_moveTask = new Thread(new ThreadStart(DragonAnimation));
+                    m_moveTask.Start();
+                }
+                else
+                {
+                    MessageBox.Show("あけましておめでとうございます。辰を止めてください。");
+                }
+
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("あけましておめでとうございます。：エラーです。" + _ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 辰を動かしながら描画する
+        /// </summary>
+        private void DragonAnimation()
+        {
+            try
+            {
+                int _startIndex = m_angles.Count - 1;
+
+                while (m_taskMoving == true)
+                {
+                    int _currentStart = Math.Max(0, _startIndex);
+                    int _currentEnd = Math.Min(m_angles.Count - 1, _startIndex + m_angles.Count / 4);
+
+                    bool _drawHead = (_startIndex >= 0);
+                    bool _drawTail = (_startIndex + m_angles.Count / 4 <= m_angles.Count - 1);
+
+                    if ((_drawHead == true && m_angles[_currentStart] == 0) || (m_angles[_currentEnd] == 0))
+                    {
+                        Image _currentImage = new Bitmap(picMain.Width, picMain.Height);
+                        List<PointF> _currentList = m_points.GetRange(_currentStart, _currentEnd - _currentStart + 1);
+                        DrawDragonBody(_currentImage, _currentList);
+                        if (_drawHead == true)
+                        {
+                            DrawDragonHead(_currentImage, _currentList.First());
+                        }
+
+                        if (_drawTail == true)
+                        {
+                            DrawDragonTail(_currentImage, _currentList.Last());
+                        }
+
+                        Invoke((MethodInvoker)(() =>
+                        {
+                            if (picMain.Image != null)
+                            {
+                                picMain.Image.Dispose();
+                                picMain.Image = null;
+                            }
+                            picMain.Image = _currentImage;
+                            Thread.Sleep(10);
+                        }
+                        ));
+                    }
+
+                    _startIndex--;
+
+                    if (_currentEnd <= 0)
+                    {
+                        _startIndex = m_angles.Count - 1;
+                    }
+                }
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("あけましておめでとうございます。：エラーです。" + _ex.Message);
+                m_taskMoving = false;
+            }
+        }
+
+        /// <summary>
+        /// 辰の顔を描画する
+        /// </summary>
+        /// <param name="image"></param>
+        private void DrawDragonHead(Image image, PointF headPoint)
+        {
+            using (Graphics _gra = Graphics.FromImage(image))
+            {
+                _gra.FillRectangle(new SolidBrush(m_bodyMain), new RectangleF(headPoint, new SizeF(m_penSize, m_penSize)));
+                _gra.FillRectangle(new SolidBrush(m_eyeAndMouthAndNose), new RectangleF(headPoint.X, headPoint.Y + 1, 1, 1));
+                _gra.FillRectangle(new SolidBrush(m_eyeAndMouthAndNose), new RectangleF(headPoint.X + 2, headPoint.Y + 1, 2, 1));
+                _gra.FillRectangle(new SolidBrush(m_eyeAndMouthAndNose), new RectangleF(headPoint.X, headPoint.Y + 3, 3, 1));
+                _gra.FillRectangle(new SolidBrush(m_antlersAndBeard), new RectangleF(headPoint.X + 2, headPoint.Y, 3, 1));
+                _gra.FillRectangle(new SolidBrush(m_antlersAndBeard), new RectangleF(headPoint.X + 4, headPoint.Y + 1, 1, 1));
+                _gra.FillRectangle(new SolidBrush(m_antlersAndBeard), new RectangleF(headPoint.X, headPoint.Y + 4, 3, 1));
+            }
+        }
+
+        /// <summary>
+        /// 辰の尻尾を描画する
+        /// </summary>
+        /// <param name="image"></param>
+        private void DrawDragonTail(Image image, PointF tailPoint)
+        {
+            using (Graphics _gra = Graphics.FromImage(image))
+            {
+                _gra.FillRectangle(new SolidBrush(m_Tail), new RectangleF(tailPoint.X - 5, tailPoint.Y, 5, 5));
+            }
+        }
+
+        /// <summary>
+        /// 辰の身体を描画する
+        /// </summary>
+        /// <param name="image"></param>
+        private void DrawDragonBody(Image image,List<PointF> bodys)
+        {
+            if (1 < bodys.Count)
+            {
+                PointF[] pointsMain = bodys.ToArray();
+                PointF[] pointsBack = bodys.ToArray();
+
+                for (int _index = 0; _index < pointsMain.Length; _index++)
+                {
+                    pointsMain[_index].Y += 2;
+                }
+                using (Graphics _gra = Graphics.FromImage(image))
+                {
+                    _gra.DrawLines(new Pen(m_bodyMain, 5), pointsMain);
+                    _gra.DrawLines(new Pen(m_bodyDark, 1), pointsBack);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 辰の身体の座標を求める
+        /// </summary>
+        /// <param name="currentLevel"></param>
+        /// <param name="length"></param>
+        private void MakeDragonBody(int currentLevel, double length)
+        {
+            if (currentLevel > 0)
+            {
+                //目標の深さに行くまで再帰します。
+                MakeDragonBody(currentLevel - 1, length / 3);//一つ深いdepthを描きながら(この中にもさらに一つ深いdepthを4回描いている)
+                Rotate(60);             //60
+                MakeDragonBody(currentLevel - 1, length / 3);
+                Rotate(-120);           //120
+                MakeDragonBody(currentLevel - 1, length / 3);
+                Rotate(60);             //60と折り返す
+                MakeDragonBody(currentLevel - 1, length / 3);
+            }
+            else if (currentLevel == 0)
+            {//目標の深さに到達したら座標が決まります
+                PointF _newPoint = new PointF();
+
+                _newPoint.X = m_points.Last().X + (float)(length * Math.Cos(m_currentAngle * Math.PI / 180.0));
+                _newPoint.Y = m_points.Last().Y + (float)(length * Math.Sin(m_currentAngle * Math.PI / 180.0));
+
+                m_points.Add(_newPoint);
+                m_angles.Add(m_currentAngle);
+            }
+        }
+
+        /// <summary>
+        /// 背景を描画する
+        /// </summary>
+        /// <param name="image"></param>
+        private void DrawBack(Image image)
+        {
+            using (Graphics _gra = Graphics.FromImage(image))
+            {
+                _gra.FillRectangle(new SolidBrush(m_sky), 0, 0, image.Width, image.Height);
+                _gra.FillPie(new SolidBrush(m_sun), image.Width / 2 - image.Height / 2, image.Height / 2, image.Height, image.Height, 180, 360);
+                _gra.FillPie(new SolidBrush(m_cloud), 0, image.Height / 2, image.Width / 5, image.Height / 10, 0, 360);
+                _gra.FillPie(new SolidBrush(m_cloud), image.Width - image.Width / 4, image.Height / 4, image.Width / 5, image.Height / 8, 0, 360);
+            }
+        }
+
+        /// <summary>
+        /// 胴体を回転させる
+        /// </summary>
+        /// <param name="rotateAngle"></param>
+        private void Rotate(double rotateAngle)
+        {
+            m_currentAngle = m_currentAngle + rotateAngle;
+            if (m_currentAngle > 360)
+            {
+                m_currentAngle = m_currentAngle - 360;
+            }
+            else if (m_currentAngle < 0)
+            {
+                m_currentAngle = m_currentAngle + 360;
+            }
+        }
+
+        /// <summary>
+        /// 画像の初期化
+        /// </summary>
+        private void InitImage()
+        {
+            // 初期化
+            if (picMain.Image != null)
+            {
+                picMain.Image.Dispose();
+                picMain.Image = null;
+            }
+            picMain.Image = new Bitmap(picMain.Width, picMain.Height);
+
+            // 初期化
+            if (picMain.BackgroundImage != null)
+            {
+                picMain.BackgroundImage.Dispose();
+                picMain.BackgroundImage = null;
+            }
+            picMain.BackgroundImage = new Bitmap(picMain.Width, picMain.Height);
+            DrawBack(picMain.BackgroundImage);
+            m_points = new List<PointF>();
+            m_points.Add(new PointF(0, 0));
+            m_angles = new List<double>();
+            m_angles.Add(0);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            m_taskMoving = false;
+        }
+    }
+}
